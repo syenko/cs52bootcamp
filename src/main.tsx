4277d832d267d32d8f7db49255bf64354c0e63b6
Alezander9 <alexander.j.yue@gmail.com>
2025-03-13 17:42:08 -0700

    feat: add clerk auth and shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
+import { ConvexReactClient } from 'convex/react'
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+        <ClerkProvider publishableKey="pk_test_bG92aW5nLW9yaW9sZS00My5jbGVyay5hY2NvdW50cy5kZXYk">
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
     <App />
+    </ConvexProviderWithClerk>
+    </ClerkProvider>
   </StrictMode>,
 )
